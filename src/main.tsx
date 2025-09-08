01b99f4f6d60b3e64df910f4ca14582bf8ceeeaa
Alessandro Marras <alessandro.marras@accenture.com>
2025-09-08 17:33:12 +0200

    feat: add vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./router/router";
 import { ThemeProvider } from "./context/ThemeProvider";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
+      <SpeedInsights />
     </ThemeProvider>
   </StrictMode>
 );
